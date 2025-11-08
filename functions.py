b59862bf4501fe26c9623ccbb65257791c4da227
IceKKL <bartlomiej.malina15@gmail.com>
2025-11-08 08:43:15 +0100

    added basic password ciphering by ceasar cipher, delegated functions into their own file, can store multiple passwords

---
+++ functions.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import string
+import pwinput
+
+def caesar(text):
+    alphabet = string.ascii_lowercase # set used
+    shifted_alphabet = alphabet[5:] + alphabet[:5] # by how many to shift the letter
+    table = str.maketrans(alphabet, shifted_alphabet) # make a table of shifts
+    return text.translate(table)
+
+def password():
+    while True:
+        # both options used for testing, will change to only "pwinput" at the end
+        # uses pwinput if the file is opened in a terminal
+        if sys.stdin.isatty():
+            passwordFirstTry = pwinput.pwinput()
+            passwordSecondTry = pwinput.pwinput("Repeat Password: ")
+        else:
+            passwordFirstTry = input("Enter your password: ")
+            passwordSecondTry = input("Repeat password: ")
+
+        if passwordFirstTry == passwordSecondTry:
+            print("Passwords match, saving")
+            save(passwordFirstTry)
+            break
+        else:
+            print("Passwords do not match")
+
+def save(password):
+    with open("passwords.txt", "a+b") as file: # open as append and binary
+        try:
+            file.seek(-2, os.SEEK_END) # set coursor to 2 bites before end of file
+            while file.read(1) != b'\n': # read one sign, if it's not new line continue
+                file.seek(-2, os.SEEK_CUR) # go another two
+        except OSError: # if file is too short, set coursor to 0
+            file.seek(0)
+        lastLine = file.readline().decode()
+
+        lineNumber = "" # get lineNumber from the file
+        for char in lastLine:
+            if char.isnumeric(): # as long as there are continuous numbers
+                lineNumber += char
+            else:
+                break
+
+        if lineNumber: # if lineNumber exists, add 1 to it
+            newLineNumber = int(lineNumber) + 1
+            file.write(("\n" + str(newLineNumber) + ". " + caesar(password)).encode("utf-8"))
+        else: # else set it to 1
+            newLineNumber = 1
+            file.write((str(newLineNumber) + ". " + caesar(password)).encode("utf-8"))
+
+
